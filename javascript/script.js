f7566f2fb39657349fb9b454c530166db9aa1dde
Joseph Racca <jracca001@student.franciscan.edu>
2026-03-25 22:19:51 -0400

    asdassd

--- javascript/script.js
+++ javascript/script.js
@@ -14,21 +14,21 @@ function enlargeImg() {
 
 /*TABLE CREATION, Code adapted from: https://www.youtube.com/watch?v=1tYjbrmsj6A*/
 
-function createTable1() {
-    fetch("table1.json")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(items) {
-            let placeholder = document.querySelector("#table1");
-            let out = "";
-            for (let item of items) {
-                out += '<tr> <td>${item.name}</td> <td>Info</td> </tr> ';
-                out += '<tr> <td>Rarity</td> <td>${item.rarity}</td> </tr> ';
-                out += '<tr> <td>Source</td> <td>${item.source}</td> </tr> ';
-                out += '<tr> <td>Drop Rate</td> <td>${item.droprate}</td> </tr> ';
-                out += '<tr> <td>Type</td> <td>${item.type}</td> </tr> ';
-            }
 
-            placeholder.innerHTML = out;
-        })
+fetch("table1.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(table1data) {
+        let placeholder = document.querySelector("#table1");
+        let out = "";
+        for (let item of table1data) {
+            out += `<tr> <td>${item.name}</td> <td>Info</td> </tr> `;
+            out += `<tr> <td>Rarity</td> <td>${item.rarity}</td> </tr> `;
+            out += `<tr> <td>Source</td> <td>${item.source}</td> </tr> `;
+            out += `<tr> <td>Drop Rate</td> <td>${item.droprate}</td> </tr> `;
+            out += `<tr> <td>Type</td> <td>${item.type}</td> </tr> `;
+        }
+
+        placeholder.innerHTML = out;
+    })
